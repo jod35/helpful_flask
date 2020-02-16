60d977e8a1bb75fe4287b6b28e5a5cbec924484f
jod35 <jodestrevin@gmail.com>
2020-02-16 01:35:28 -0500

    added a static CSS

---
+++ 8.forms/app.py
@@ -0,0 +1,56 @@
+from flask import Flask,render_template #this method is used to render templates
+from flask_wtf import FlaskForm #flask extension for Wtforms
+from wtforms import StringField,PasswordField,SubmitField,TextAreaField #the needed fields
+from wtforms.validators import DataRequired,Email,EqualTo
+app=Flask(__name__)
+
+#fake in memory database containing post data
+
+
+
+posts=[
+   {
+      'author':"Harry Porter",
+      'date_posted':"Sat 15 Feb 2020",
+      'title':"A day in Portland",
+      'content':"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptate, quis quae iure, hic accusamus culpa minima nam non eius praesentium earum veniam molestias sed delectus eum odit tenetur voluptatem."
+   },
+   {
+      'author':"John Doe",
+      'date_posted':"Sat 15 Feb 2020",
+      'title':"How was Valentine?",
+      'content':"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptate, quis quae iure, hic accusamus culpa minima nam non eius praesentium earum veniam molestias sed delectus eum odit tenetur voluptatem."
+   },
+   {
+      'author':"Jane Doe",
+      'date_posted':"Sat 15 Feb 2020",
+      'title':"One African survived coronavirus",
+      'content':"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptate, quis quae iure, hic accusamus culpa minima nam non eius praesentium earum veniam molestias sed delectus eum odit tenetur voluptatem."
+   },
+
+]
+
+@app.route('/')
+def index():
+
+   return render_template('index.html',posts=posts)
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('signup')
+def create_aacount():
+   return render_template('signup.html')
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
